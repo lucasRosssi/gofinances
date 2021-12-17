8d5527d1aae9c61b83a51e04cbed6ba13e5778ef
lucasRosssi <lucasrm2305@gmail.com>
2021-12-17 12:14:24 -0300

    change color values to theme values

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,11 +4,12 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: #eeeeee;
+
+    background-color: ${({ theme }) => theme.colors.background};
 `
 
 export const Title = styled.Text`
     font-size: 24px;
     font-weight: bold;
-    color: purple;
+    color: ${({ theme }) => theme.colors.title};
 `
