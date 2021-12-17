250bd777fec1d80da17b45ff367f703a98f5bfde
lucasRosssi <lucasrm2305@gmail.com>
2021-12-17 12:30:18 -0300

    add poppins font and make related adjustments

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Dashboard />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    );
+  }
+  
 }
 
 
