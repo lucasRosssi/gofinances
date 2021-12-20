08d26ee514600762f0fb6c80963ceab8211f613d
lucasRosssi <lucasrm2305@gmail.com>
2021-12-20 11:03:33 -0300

    add TransactionCard component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
 import { 
     Container,
@@ -11,7 +12,9 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title
 } from "./styles";
 
 export function Dashboard() {
@@ -51,6 +54,12 @@ export function Dashboard() {
                     type="total"
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionCard />
+            </Transactions>
         </Container>
     )
 }
