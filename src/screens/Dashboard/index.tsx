7e3a59469823611852eaf1453d2065bbb9c0b428
lucasRosssi <lucasrm2305@gmail.com>
2021-12-29 17:43:44 -0300

    add flatlist to transaction list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+
 import { HighlightCard } from "../../components/HighlightCard";
 import { TransactionCard } from "../../components/TransactionCard";
 
@@ -14,10 +16,44 @@ import {
     Icon,
     HighlightCards,
     Transactions,
-    Title
+    Title,
+    TransactionList
 } from "./styles";
 
 export function Dashboard() {
+    const data = [
+        {
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2021"
+        },
+        {
+            type: 'negative',
+            title: "Hamburgueria Pizzy",
+            amount: "R$ 59,00",
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: "10/04/2021"
+        },
+        {
+            type: 'negative',
+            title: "Aluguel do apartamento",
+            amount: "R$ 1.200,00",
+            category: {
+                name: 'Casa',
+                icon: 'home'
+            },
+            date: "10/04/2021"
+        }
+    ]
+
     return (
         <Container>
             <Header>
@@ -58,7 +94,17 @@ export function Dashboard() {
             <Transactions>
                 <Title>Listagem</Title>
 
-                <TransactionCard />
+                <TransactionList
+                    data={data}
+                    renderItem={({ item }) => 
+                        <TransactionCard  data={item} />
+                    }
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace()
+                    }}
+                />
+
             </Transactions>
         </Container>
     )
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -80,3 +80,7 @@ export const Title = styled.Text`
 
     margin-bottom: 16px;
 `
+
+export const TransactionList = styled.FlatList`
+
+`
