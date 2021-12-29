e4cb9713dab583953a62288da36c66334231e193
lucasRosssi <lucasrm2305@gmail.com>
2021-12-29 19:19:44 -0300

    added Button to Register screen

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Input } from "../../components/Forms/Input";
+import { Button } from "../../components/Forms/Button";
 
 import { 
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields
 } from "./styles";
 
 export function Register() {
@@ -17,12 +19,17 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input 
-                    placeholder="Nome"
-                />
-                <Input 
-                    placeholder="Preço"
-                />
+                <Fields>
+                    <Input 
+                        placeholder="Nome"
+                    />
+                    <Input 
+                        placeholder="Preço"
+                    />
+
+                </Fields>
+
+                <Button title="Enviar" />
             </Form>
 
         </Container>
