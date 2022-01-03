e774cbfa056d6d9ecc027817570ce2d867570c1f
lucasRosssi <lucasrm2305@gmail.com>
2022-01-03 11:12:51 -0300

    created CategorySelect modal

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import theme from './src/global/styles/theme'
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
   } else {
     return (
       <ThemeProvider theme={theme}>
-        <Register />
+        <CategorySelect />
       </ThemeProvider>
     );
   }
