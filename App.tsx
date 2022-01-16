d7162af291d5248b649dbde1633a4d7f08b983b0
lucasRosssi <lucasrm2305@gmail.com>
2022-01-16 17:42:33 -0300

    import intl

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import 'react-native-gesture-handler'
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
+import React from 'react'
+import AppLoading from 'expo-app-loading'
 import theme from './src/global/styles/theme'
-import { ThemeProvider } from 'styled-components';
-import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components'
+import { NavigationContainer } from '@react-navigation/native'
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
+  Poppins_700Bold,
 } from '@expo-google-fonts/poppins'
 
-import { AppRoutes } from './src/routes/app.routes';
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   })
 
   if (!fontsLoaded) {
@@ -29,9 +33,6 @@ export default function App() {
           <AppRoutes />
         </NavigationContainer>
       </ThemeProvider>
-    );
+    )
   }
-  
 }
-
-
