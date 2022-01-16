e8394dab56671a4b39e69350734cc92047e52246
lucasRosssi <lucasrm2305@gmail.com>
2022-01-16 17:45:53 -0300

    worked on dashboard list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,118 +1,127 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { HighlightCard } from "../../components/HighlightCard";
-import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
+import { HighlightCard } from '../../components/HighlightCard'
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from '../../components/TransactionCard'
 
-import { 
-    Container,
-    Header,
-    UserWrapper,
-    UserInfo,
-    Photo,
-    User,
-    UserGreeting,
-    UserName,
-    LogoutButton,
-    Icon,
-    HighlightCards,
-    Transactions,
-    Title,
-    TransactionList
-} from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  LogoutButton,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
+} from './styles'
+import { useFocusEffect } from '@react-navigation/native'
 
 export interface DataListProps extends TransactionCardProps {
-    id: string
+  id: string
 }
 
 export function Dashboard() {
-    const data: DataListProps[] = [
-        {
-            id: '1',
-            type: 'positive',
-            title: "Desenvolvimento de site",
-            amount: "R$ 12.000,00",
-            category: {
-                name: 'Vendas',
-                icon: 'dollar-sign'
-            },
-            date: "13/04/2021"
-        },
-        {
-            id: '2',
-            type: 'negative',
-            title: "Hamburgueria Pizzy",
-            amount: "R$ 59,00",
-            category: {
-                name: 'Alimentação',
-                icon: 'coffee'
-            },
-            date: "10/04/2021"
-        },
-        {
-            id: '3',
-            type: 'negative',
-            title: "Aluguel do apartamento",
-            amount: "R$ 1.200,00",
-            category: {
-                name: 'Casa',
-                icon: 'home'
-            },
-            date: "10/04/2021"
+  const [data, setData] = useState<DataListProps[]>([])
+
+  async function loadTransactions() {
+    const dataKey = '@gofinances:transactions'
+    const response = await AsyncStorage.getItem(dataKey)
+    const transactions = response ? JSON.parse(response) : []
+
+    const transactionsFormatted: DataListProps[] = transactions.map(
+      (item: DataListProps) => {
+        const amount = Number(item.amount).toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })
+
+        const date = Intl.DateTimeFormat('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit',
+        }).format(new Date(item.date))
+
+        return {
+          id: item.id,
+          name: item.name,
+          amount,
+          type: item.type,
+          category: item.category,
+          date,
         }
-    ]
-
-    return (
-        <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo source={{ uri: 'https://github.com/lucasRosssi.png' }}/>
-                        <User>
-                            <UserGreeting>Olá,</UserGreeting>
-                            <UserName>Lucas</UserName>
-                        </User>
-                    </UserInfo>
-
-                    <LogoutButton onPress={() => {}}>
-                        <Icon name="power" />
-                    </LogoutButton>
-                </UserWrapper>
-            </Header>
-
-            <HighlightCards>
-                <HighlightCard 
-                    title="Entradas" 
-                    amount="R$ 17.400,00"
-                    lastTransaction="Última entrada dia 13 de abril"
-                    type="up"
-                />
-                <HighlightCard 
-                    title="Saídas"
-                    amount="R$ 1.259,00"
-                    lastTransaction="Última saída dia 03 de abril"
-                    type="down"
-                />
-                <HighlightCard 
-                    title="Total"
-                    amount="R$ 16.141,00"
-                    lastTransaction="01 a 16 de abril"
-                    type="total"
-                />
-            </HighlightCards>
-
-            <Transactions>
-                <Title>Listagem</Title>
-
-                <TransactionList
-                    data={data}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => 
-                        <TransactionCard  data={item} />
-                    }
-                />
-
-            </Transactions>
-        </Container>
+      }
     )
-}
 
+    setData(transactionsFormatted)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+  useFocusEffect(
+    useCallback(() => {
+      loadTransactions()
+    }, [])
+  )
+
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/lucasRosssi.png' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>
+
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 a 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
+    </Container>
+  )
+}
