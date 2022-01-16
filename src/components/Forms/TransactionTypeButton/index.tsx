0bf61cb535b1a678f5af3bac6d71d1cfad1d3c26
lucasRosssi <lucasrm2305@gmail.com>
2022-01-16 17:44:13 -0300

    type value fix

--- src/components/Forms/TransactionTypeButton/index.tsx
+++ src/components/Forms/TransactionTypeButton/index.tsx
@@ -1,51 +1,41 @@
-import React from "react";
+import React from 'react'
 
-import { RectButtonProps } from "react-native-gesture-handler";
+import { RectButtonProps } from 'react-native-gesture-handler'
 
-import { 
-    Container,
-    Button,
-    Icon,
-    Title 
-} from "./styles";
+import { Container, Button, Icon, Title } from './styles'
 
 const icons = {
-    up: 'arrow-up-circle',
-    down: 'arrow-down-circle'
+  positive: 'arrow-up-circle',
+  negative: 'arrow-down-circle',
 }
 
 interface Props extends RectButtonProps {
-    type: 'up' | 'down',
-    title: string,
-    isActive: boolean,
-    isInactive: boolean
+  type: 'positive' | 'negative'
+  title: string
+  isActive: boolean
+  isInactive: boolean
 }
 
-export function TransactionTypeButton({ 
-    title, 
-    type,
-    isActive,
-    isInactive, 
-    ...rest 
+export function TransactionTypeButton({
+  title,
+  type,
+  isActive,
+  isInactive,
+  ...rest
 }: Props) {
-
-    return (
-        <Container
-            type={type}
-            isActive={isActive}
-            isInactive={isInactive}
-        >
-            <Button {...rest}>
-                <Icon 
-                    name={icons[type]}
-                    type={type}
-                    isActive={isActive}
-                    isInactive={isInactive}
-                />
-                <Title isActive={isActive} isInactive={isInactive}>
-                    {title}
-                </Title>
-            </Button>
-        </Container>
-    )
+  return (
+    <Container type={type} isActive={isActive} isInactive={isInactive}>
+      <Button {...rest}>
+        <Icon
+          name={icons[type]}
+          type={type}
+          isActive={isActive}
+          isInactive={isInactive}
+        />
+        <Title isActive={isActive} isInactive={isInactive}>
+          {title}
+        </Title>
+      </Button>
+    </Container>
+  )
 }
--- src/components/Forms/TransactionTypeButton/styles.ts
+++ src/components/Forms/TransactionTypeButton/styles.ts
@@ -1,72 +1,91 @@
-import styled, { css } from "styled-components/native";
-import { RFValue } from "react-native-responsive-fontsize";
+import styled, { css } from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
 
-import { Feather } from "@expo/vector-icons"
-import { RectButton } from "react-native-gesture-handler";
+import { Feather } from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
 
 interface TransactionButtonProps {
-    isActive?: boolean,
-    isInactive?: boolean,
-    type?: 'up' | 'down'
+  isActive?: boolean
+  isInactive?: boolean
+  type?: 'positive' | 'negative'
 }
 
 export const Container = styled.View<TransactionButtonProps>`
-    width: 48%;
+  width: 48%;
 
-    border-width: ${({ isActive }) => isActive ? 0 : 1.5}px;
-    border-style: solid;
-    border-color: ${({ theme }) => theme.colors.text_light};
-    border-radius: 5px;
+  border-width: ${({ isActive }) => (isActive ? 0 : 1.5)}px;
+  border-style: solid;
+  border-color: ${({ theme }) => theme.colors.text_light};
+  border-radius: 5px;
 
-    ${({ isActive, type }) => isActive && type === 'up' && css`
-        background-color: ${({ theme }) => theme.colors.success};
+  ${({ isActive, type }) =>
+    isActive &&
+    type === 'positive' &&
+    css`
+      background-color: ${({ theme }) => theme.colors.success};
     `};
 
-    ${({ isActive, type }) => isActive && type === 'down' && css`
-        background-color: ${({ theme }) => theme.colors.attention};
+  ${({ isActive, type }) =>
+    isActive &&
+    type === 'negative' &&
+    css`
+      background-color: ${({ theme }) => theme.colors.attention};
     `};
 
-    ${({ isInactive }) => isInactive && css`
-        border-color: ${({ theme }) => theme.colors.background};
+  ${({ isInactive }) =>
+    isInactive &&
+    css`
+      border-color: ${({ theme }) => theme.colors.background};
     `};
 `
 
 export const Button = styled(RectButton)<TransactionButtonProps>`
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
-    padding: 16px;
-`;
+  padding: 16px;
+`
 
 export const Icon = styled(Feather)<TransactionButtonProps>`
-    font-size: ${RFValue(24)}px;
-    margin-right: 12px;
+  font-size: ${RFValue(24)}px;
+  margin-right: 12px;
 
-    ${({ type }) => type === 'up' 
-        ? css`color: ${({ theme }) => theme.colors.success}` 
-        : css`color: ${({ theme }) => theme.colors.attention}`
-    };
+  ${({ type }) =>
+    type === 'positive'
+      ? css`
+          color: ${({ theme }) => theme.colors.success};
+        `
+      : css`
+          color: ${({ theme }) => theme.colors.attention};
+        `};
 
-    ${({ isActive }) => isActive && css`
-        color: ${({ theme }) => theme.colors.shape};
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      color: ${({ theme }) => theme.colors.shape};
     `};
 
-    ${({ isInactive }) => isInactive && css`
-        opacity: 0.3;
+  ${({ isInactive }) =>
+    isInactive &&
+    css`
+      opacity: 0.3;
     `};
 `
 
 export const Title = styled.Text<TransactionButtonProps>`
-    font-family: ${({ theme }) => theme.fonts.regular};
-    font-size: ${RFValue(14)}px;
-    color: ${({ theme }) => theme.colors.text_dark}
-
-    ${({ isActive }) => isActive && css`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text_dark}
+    ${({ isActive }) =>
+      isActive &&
+      css`
         color: ${({ theme }) => theme.colors.shape};
-    `};
+      `};
 
-    ${({ isInactive }) => isInactive && css`
-        opacity: 0.3;
+  ${({ isInactive }) =>
+    isInactive &&
+    css`
+      opacity: 0.3;
     `};
 `
