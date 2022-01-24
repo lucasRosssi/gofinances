2a6e1346da960bbc4218228cda0e52127803a803
lucasRosssi <lucasrm2305@gmail.com>
2022-01-24 11:05:51 -0300

    added Resume screen to routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,6 +6,7 @@ import { Platform } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { Dashboard } from '../screens/Dashboard'
 import { Register } from '../screens/Register'
+import { Resume } from '../screens/Resume'
 
 export type RootStackParamList = {
   Listagem: undefined
@@ -55,7 +56,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />
