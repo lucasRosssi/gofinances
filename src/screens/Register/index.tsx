cfa53fbbd8af71c5e859ea7bd9ec6cab67af4cea
lucasRosssi <lucasrm2305@gmail.com>
2022-01-24 11:36:28 -0300

    minor adjustment

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -85,7 +85,6 @@ export function Register() {
 
     try {
       const dataKey = '@gofinances:transactions'
-
       const data = await AsyncStorage.getItem(dataKey)
       const currentData = data ? JSON.parse(data) : []
 
