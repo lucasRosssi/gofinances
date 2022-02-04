a3646bba89fc5ff07c45361a3ba2f9c80f5d51d1
lucasRosssi <lucasrm2305@gmail.com>
2022-02-03 23:20:16 -0300

    removed unnecessary interface

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -3,20 +3,16 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Feather } from '@expo/vector-icons'
-import theme from "../../global/styles/theme";
-
-interface CategoryProps {
-    isActive: boolean
-}
 
 export const Container = styled(GestureHandlerRootView)`
-    flex: 1;
+    width: 90%;
+		height: 70%;
     background-color: ${({ theme }) => theme.colors.background};
 `
 
 export const Header = styled.View`
     width: 100%;
-    height: ${RFValue(113)}px;
+    height: ${RFValue(70)}px;
 
     background-color: ${({ theme }) => theme.colors.primary};
 
@@ -31,17 +27,12 @@ export const Title = styled.Text`
     color: ${({ theme }) => theme.colors.shape};
 `
 
-export const Category = styled.TouchableOpacity<CategoryProps>`
+export const Category = styled.TouchableOpacity`
     width: 100%;
-    padding: ${RFValue(15)}px;
+    padding: ${RFValue(10)}px ${RFValue(15)}px;
 
     flex-direction: row;
     align-items: center;
-
-    background-color: ${({ isActive }) => isActive 
-        ? theme.colors.secondary_light
-        : theme.colors.background
-    };
 `
 
 export const Icon = styled(Feather)`
