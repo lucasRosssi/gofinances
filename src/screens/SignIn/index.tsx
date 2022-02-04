0781106e8e3de6a38c871860803a112c192857cf
lucasRosssi <lucasrm2305@gmail.com>
2022-02-03 23:22:05 -0300

    added loading indicator

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useAuth } from "../../hooks/auth";
 
-import { Alert } from "react-native";
+import { ActivityIndicator, Alert, Platform } from "react-native";
 import AppleSvg from '../../assets/apple.svg'
 import GoogleSvg from '../../assets/google.svg'
 import LogoSvg from '../../assets/logo.svg'
@@ -17,29 +17,37 @@ import {
 	Footer,
 	ButtonWrapper
 } from "./styles";
+import { useTheme } from "styled-components";
 
 export function SignIn() {
+	const [isLoading, setIsLoading] = useState(false)
+
 	const { signInWithGoogle, signInWithApple } = useAuth()
+	const theme = useTheme()
 	
 	async function handleSignInWithGoogle() {
 		try {
+			setIsLoading(true)
 			await signInWithGoogle()
 
 		} catch(error) {
 			console.log(error)
 			Alert.alert('Não foi possível conectar a conta Google')
 		}
+		setIsLoading(false)
 	}
 
 	async function handleSignInWithApple() {
 		try {
+			setIsLoading(true)
 			await signInWithApple()
 
 		} catch(error) {
 			console.log(error)
 			Alert.alert('Não foi possível conectar a conta Apple')
 		}
-	}
+		setIsLoading(false)
+	} 
 
 	return (
 		<Container>
@@ -69,14 +77,26 @@ export function SignIn() {
 						title="Entrar com Google"
 						svg={GoogleSvg}
 						onPress={handleSignInWithGoogle}
+						enabled={!isLoading}
 					/>
 
-					<SignInSocialButton
-						title="Entrar com Apple"
-						svg={AppleSvg}
-						onPress={handleSignInWithApple}
-					/>
+					{ Platform.OS === 'ios' &&
+						<SignInSocialButton
+							title="Entrar com Apple"
+							svg={AppleSvg}
+							onPress={handleSignInWithApple}
+							enabled={!isLoading}
+						/>
+					}
+					{ isLoading && 
+						<ActivityIndicator
+							color={theme.colors.shape}
+							size="large"
+							style={{ margin: 18 }}
+						/>
+					}
 				</ButtonWrapper>
+
 
 			</Footer>
 
