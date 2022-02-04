901c94f0b4d66100622bcf7e27ceb76debbff8a0
lucasRosssi <lucasrm2305@gmail.com>
2022-02-03 23:22:14 -0300

    added new categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,8 +1,12 @@
 export const categories = [
-    { key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#5636D3' },
-    { key: 'food', name: 'Alimentação', icon: 'coffee', color: '#FF872C' },
-    { key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#12A454' },
-    { key: 'car', name: 'Carro', icon: 'crosshair', color: '#E83F5B' },
-    { key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195C' },
-    { key: 'studies', name: 'Estudos', icon: 'book', color: '#9C001A' },
+	{ key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#56AB26' },
+	{ key: 'investment', name: 'Investimento', icon: 'percent', color: '#BFAF00' },
+	{ key: 'bills', name: 'Contas', icon: 'credit-card', color: '#9E9E9E' },
+	{ key: 'food', name: 'Alimentação', icon: 'coffee', color: '#FF872C' },
+	{ key: 'health', name: 'Saúde', icon: 'activity', color: '#9C001A' },
+	{ key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#5636D3' },
+	{ key: 'house', name: 'Casa', icon: 'home', color: '#7F12C7' },
+	{ key: 'car', name: 'Carro', icon: 'crosshair', color: '#E83F5B' },
+	{ key: 'leisure', name: 'Lazer', icon: 'heart', color: '#0089BF' },
+	{ key: 'studies', name: 'Estudos', icon: 'book', color: '#AB269E' },
 ]; 
