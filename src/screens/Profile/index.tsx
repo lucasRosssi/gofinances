ab78d5839bcd67e7e84c7adc0f04281138f1961a
lucasRosssi <lucasrm2305@gmail.com>
2022-03-31 12:28:00 -0300

    created screen for test

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
+export function Profile() {
+	return (
+		<View>
+			<Text testID="text-title">Perfil</Text>
+
+			<TextInput
+				testID="input-name"
+				placeholder="Nome"
+				autoCorrect={false}
+				value="Lucas"
+			/>
+			<TextInput testID="input-surname" placeholder="Sobrenome" value="Rossi" />
+
+			<Button title="Salvar" onPress={() => {}} />
+		</View>
+	);
+}
