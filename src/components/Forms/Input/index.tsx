a15b47c6512d6d992a3f395f4dbc30ad581ffce2
lucasRosssi <lucasrm2305@gmail.com>
2022-03-31 19:36:09 -0300

    refactored for test

--- src/components/Forms/Input/index.tsx
+++ src/components/Forms/Input/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { TextInputProps } from "react-native";
+import React from 'react';
+import { TextInputProps } from 'react-native';
 
-import { 
-    Container 
-} from "./styles";
+import { Container } from './styles';
 
-type Props = TextInputProps
+interface Props extends TextInputProps {
+	active?: boolean;
+}
 
-export function Input({...rest}: Props) {
-    return (
-        <Container {...rest} />
-    )
+export function Input({ active = false, ...rest }: Props) {
+	return <Container active={active} {...rest} />;
 }
