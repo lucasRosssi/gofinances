1e4a5c2b8a3e9231af37ad5f589a3e91230c9b2d
lucasRosssi <lucasrm2305@gmail.com>
2022-04-05 19:14:48 -0300

    imported theme directly to work in test

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,80 +1,89 @@
-import React from "react";
-import { FlatList } from "react-native-gesture-handler";
-import { useTheme } from "styled-components";
+import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
 
-import { Button } from "../../components/Forms/Button";
+import { Button } from '../../components/Forms/Button';
+import theme from '../../global/styles/theme';
 
-import { categories } from "../../utils/categories";
+import { categories } from '../../utils/categories';
 
-import { 
-    Container,
-    Header,
-    Title,
-    Category,
-    Icon,
-    Name,
-    Separator,
-    Footer
-} from "./styles";
+import {
+	Container,
+	Header,
+	Title,
+	Category,
+	Icon,
+	Name,
+	Separator,
+	Footer,
+} from './styles';
 
 interface Category {
-    key: string,
-    name: string
+	key: string;
+	name: string;
 }
 
 interface Props {
-    category: Category,
-    setCategory: (category: Category) => void,
-    closeSelectCategory: () => void
+	category: Category;
+	setCategory: (category: Category) => void;
+	closeSelectCategory: () => void;
 }
 
-export function CategorySelect({ 
-    category,
-    setCategory,
-    closeSelectCategory
- }: Props) {
-	const theme = useTheme()
+export function CategorySelect({
+	category,
+	setCategory,
+	closeSelectCategory,
+}: Props) {
+	function handleCategorySelect(category: Category) {
+		setCategory(category);
+	}
 
-    function handleCategorySelect(category: Category) {
-        setCategory(category)
-    }
+	return (
+		<Container>
+			<Header>
+				<Title>Categorias</Title>
+			</Header>
 
-    return (
-    <Container>
-        <Header>
-            <Title>Categorias</Title>
-        </Header>
+			<FlatList
+				data={categories}
+				style={{ flex: 1, width: '100%' }}
+				keyExtractor={(item) => item.key}
+				renderItem={({ item }) => (
+					<Category
+						onPress={() => handleCategorySelect(item)}
+						style={{
+							backgroundColor:
+								category.key === item.key
+									? item.color
+									: theme.colors.background,
+						}}
+					>
+						<Icon
+							name={item.icon}
+							color={
+								category.key === item.key
+									? theme.colors.shape
+									: theme.colors.text_dark
+							}
+						/>
+						<Name
+							style={{
+								color:
+									category.key === item.key
+										? theme.colors.shape
+										: theme.colors.text_dark,
+							}}
+						>
+							{item.name}
+						</Name>
+					</Category>
+				)}
+				ItemSeparatorComponent={() => <Separator />}
+				showsVerticalScrollIndicator={false}
+			/>
 
-        <FlatList 
-            data={categories}
-            style={{ flex: 1, width: '100%' }}
-            keyExtractor={(item) => item.key}
-            renderItem={({ item }) => (
-                <Category
-                    onPress={() => handleCategorySelect(item)}
-										style={{ backgroundColor: category.key === item.key ? item.color : theme.colors.background }}
-                >
-                    <Icon name={item.icon} color={category.key === item.key ? theme.colors.shape : theme.colors.text_dark} />
-                    <Name
-											style={{
-												color: category.key === item.key ? theme.colors.shape : theme.colors.text_dark
-											}}
-										>
-											{item.name}
-										</Name>
-                </Category>
-            )}
-            ItemSeparatorComponent={() => <Separator />}
-						showsVerticalScrollIndicator={false}
-        />
-
-        <Footer>
-            <Button 
-                title="Selecionar"
-                onPress={closeSelectCategory}
-            />
-        </Footer>
-
-    </Container>
-    )
+			<Footer>
+				<Button title="Selecionar" onPress={closeSelectCategory} />
+			</Footer>
+		</Container>
+	);
 }
