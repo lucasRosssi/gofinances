1fabea2951772f6b2573dbf78a22af3b7f50f366
lucasRosssi <lucasrm2305@gmail.com>
2022-04-05 19:14:31 -0300

    extended interface

--- src/components/Forms/CategorySelectButton/index.tsx
+++ src/components/Forms/CategorySelectButton/index.tsx
@@ -1,22 +1,18 @@
-import React from "react";
+import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
-import { 
-    Container,
-    Category,
-    Icon 
-} from "./styles";
+import { Container, Category, Icon } from './styles';
 
-interface Props {
-    title: string,
-    onPress: () => void
+interface Props extends RectButtonProps {
+	title: string;
+	onPress: () => void;
 }
 
-export function CategorySelectButton({ title, onPress }: Props) {
-    return (
-        <Container onPress={onPress}>
-            <Category>{title}</Category>
-            <Icon name="chevron-down" />
-
-        </Container>
-    )
+export function CategorySelectButton({ title, onPress, ...rest }: Props) {
+	return (
+		<Container onPress={onPress} {...rest}>
+			<Category>{title}</Category>
+			<Icon name="chevron-down" />
+		</Container>
+	);
 }
