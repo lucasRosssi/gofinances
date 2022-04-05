149ee06fcaa9b3fcce53cc3e8596b42a3ec1a837
lucasRosssi <lucasrm2305@gmail.com>
2022-04-05 19:14:17 -0300

    created register test

---
+++ src/__tests__/screens/register.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '../../global/styles/theme';
+
+import { Register } from '../../screens/Register';
+
+const Providers: React.FC = ({ children }) => (
+	<ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+describe('Register Screen', () => {
+	it('should open category modal on button press', () => {
+		const { getByTestId } = render(<Register />, { wrapper: Providers });
+
+		const categoryModal = getByTestId('modal-category');
+		const buttonCategory = getByTestId('button-category');
+		fireEvent.press(buttonCategory);
+
+		expect(categoryModal.props.visible).toBeTruthy();
+	});
+});
